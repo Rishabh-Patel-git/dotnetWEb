c52f21ee88511ace05bffe8f6d3410e91966146f
Rishabh Patel <rishabh.patel@nagarro.com>
2023-03-27 09:13:56 +0530

    Assignment 2 first commit

---
+++ Assignment2_webApp/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Assignment2_webApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("calculate")]
+        public IActionResult Index(double num1,double num2,string operation)
+        {
+            double result;
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    if(num2 == 0)
+                    {
+                        ViewBag.ErrorMsg = "Cannot divide by Zero";
+                        return View();
+                    }
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if(num2 == 0)
+                    {
+                        ViewBag.ErrorMsg = "Cannot divide by Zero";
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    ViewBag.ErrorMsg = "Invalid operator selected";
+                    return View();
+            }
+            string ans = num1 + " " + operation + " " + num2 + " = " + result;
+            ViewBag.Result = ans;
+            return View();
+        }
+    }
+}
